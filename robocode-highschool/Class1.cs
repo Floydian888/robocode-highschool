fd11300404e992ae0fd39a929ddc3e69fb988412
Albert Bagieński <albert.bagienski@gmail.com>
2014-11-28 16:48:04 +0100

    proba dodania strzelania i celowania na razie na slepo, bo nie moge kompilowac

--- robocode-highschool/Class1.cs
+++ robocode-highschool/Class1.cs
@@ -20,13 +20,35 @@ namespace robocode_highschool
                 //tutaj wchodzi zapętlone wszystko, co robi potem, chodzenie, ruszanie lufą itp...
             }
         }
-             // Robot event handler, when the robot sees another robot
-            public override void OnScannedRobot(ScannedRobotEvent e)
-            {
-                 // We fire the gun with bullet power = 1
+
+
+       public override void ourrobotFire(double distance) //pyk! strzelanie naszym robotem z rozna sila
+       {
+          // distance okresla odleglosc robota od zeskanowanego przeciwnika
+           if (distance > 200 || getEnergy() < 20) // gdy mamy malo energii lub przeciwnik jest daleko, strzelamy z mala sila (wtedy pocisk ksztuje malo energii, zadaje malo dmg, ale szybko leci)
+               Fire(1); 
+           else
+               if (distance < 50) // jak przeciwnik jest jakos sensownie blisko i mamy  to strzelamy z wieksza sila 
+                    Fire(2);
+               else
+                   if (distance < 30) // a jak jest bardzo blisko, to nawalamy z calej pary
+                       Fire(3);
+
+        }
+
+       public override void targetPractice(ScannedRobotEvent e) // proba wycelowania w naszego przeciwnika
+       {
+           TurnGunRight(getHeading() - getGunHeading() + e.getBearing()); // zasadniczo przesuwamy armate w te strone, z ktorej widzimy przeciwnika
+       }
+
+       // Robot event handler, when the robot sees another robot
+       public override void OnScannedRobot(ScannedRobotEvent e)
+       {
+           targetPractice(e);
+           // tu dodac strzelanie
                  
                 //można też przypisywać instrukcje do wszystkich rzeczy, które mogą się przydarzyć naszemu robotowi
                 //w tym przypadku jest to zauważenie innego robota, po którym warto strzelić ;)
-            }
-     }
+       }
+   }
 }
